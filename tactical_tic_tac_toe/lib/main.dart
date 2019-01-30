b00cc76c1d4165bb73290c8774407736977a8157
Moesen <gustavmoesmand@gmail.com>
2019-01-30 19:06:14 +0100

    AppBar progress

--- tactical_tic_tac_toe/lib/main.dart
+++ tactical_tic_tac_toe/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Board.dart';
+import 'presentation/custom_icons.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,33 +15,71 @@ class Game extends StatelessWidget {
   Widget build(BuildContext context) {
     // Scaffold is a layout for the major Material Components.
     return Scaffold(
-      appBar: defaultBar("Play!"),
+      appBar: ,
       body: board.getBoard(),
     );
   }
 }
 
 class Home extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: defaultBar("Tactical Tic Tac Toe"),
-      body:
+      appBar: ,
+        body: Center(
+        child: Column (
+          children: <Widget> [
+            Hero(
+            tag: 'goToButton',
+            child: IconButton(
+              icon: Icon(CustomIcons.vkontakte),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Game()),
+                );
+              },
+            ),
+            ),
+            Text("Go to Game"),
+          ],
+        ),
       ),
     );
   }
 
 }
 
-class defaultBar extends AppBar {
-  defaultBar(String title):
-      super(
-        elevation: 0.5,
-        title: new Text(title,
-          style: TextStyle(fontFamily: 'Krub',
-              fontWeight: FontWeight.bold, color: Colors.black.withOpacity(0.8)),
-        ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-      );
+class TicTacBar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    _TicTacBarState createState() => _TicTacBarState();
+  }
 }
+
+class _TicTacBarState extends State<TicTacBar> {
+  Choice _selectedChoice = choices[0];
+
+  void _select(Choice choice) {
+    setState(() {
+      _selectedChoice = choice;
+    });
+  }
+}
+
+class Choice {
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Car', icon: Icons.directions_car),
+  const Choice(title: 'Bicycle', icon: Icons.directions_bike),
+  const Choice(title: 'Boat', icon: Icons.directions_boat),
+  const Choice(title: 'Bus', icon: Icons.directions_bus),
+  const Choice(title: 'Train', icon: Icons.directions_railway),
+  const Choice(title: 'Walk', icon: Icons.directions_walk),
+];
